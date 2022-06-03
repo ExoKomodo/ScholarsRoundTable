0785c996a2b50865722a72ac55ea570d8895d7b9
James Orson <jorson@purestorage.com>
2022-06-02 18:16:23 -0700

    Update Flask-Login

--- src/setup.py
+++ src/setup.py
@@ -8,7 +8,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'Flask-Login==0.4.1',
+        'Flask-Login==0.6.1',
         'Flask==2.1.0',
         'mysql-connector==2.2.9',
         'oauthlib==3.1.0',
