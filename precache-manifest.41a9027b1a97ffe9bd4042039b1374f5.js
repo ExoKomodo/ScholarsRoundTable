6ef279bad7b2deaf3e74254035053ef1c07432b7
JamesOrson <JamesOrson@users.noreply.github.com>
2022-06-13 15:01:37 +0000

    deploy: 34d4cfa255775905b52d0c8967ed5aa5bd37fba6

---
+++ precache-manifest.41a9027b1a97ffe9bd4042039b1374f5.js
@@ -0,0 +1,690 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4aaf95b30dbb2f932be",
+    "url": "/static/css/app.8e2c6bf8.css"
+  },
+  {
+    "revision": "e4aaf95b30dbb2f932be",
+    "url": "/static/js/app.6a62c872.js"
+  },
+  {
+    "revision": "6d96b367d49d083ba5d6",
+    "url": "/static/css/chunk-02705be5.cd0aadc4.css"
+  },
+  {
+    "revision": "6d96b367d49d083ba5d6",
+    "url": "/static/js/chunk-02705be5.f1fe43f9.js"
+  },
+  {
+    "revision": "282810687f8a9d46235b",
+    "url": "/static/css/chunk-184d01f6.48639f1c.css"
+  },
+  {
+    "revision": "282810687f8a9d46235b",
+    "url": "/static/js/chunk-184d01f6.4bded4f6.js"
+  },
+  {
+    "revision": "f67387182dcecd12a059",
+    "url": "/static/css/chunk-1dafbdeb.b736741f.css"
+  },
+  {
+    "revision": "f67387182dcecd12a059",
+    "url": "/static/js/chunk-1dafbdeb.c609f0c6.js"
+  },
+  {
+    "revision": "7e32a927b38903defd9d",
+    "url": "/static/css/chunk-1dbb0e8c.abf0cc24.css"
+  },
+  {
+    "revision": "7e32a927b38903defd9d",
+    "url": "/static/js/chunk-1dbb0e8c.566feacc.js"
+  },
+  {
+    "revision": "a75bc28b755024483196",
+    "url": "/static/css/chunk-2df7371d.72a064be.css"
+  },
+  {
+    "revision": "a75bc28b755024483196",
+    "url": "/static/js/chunk-2df7371d.eaed8031.js"
+  },
+  {
+    "revision": "2221d46ae508aec7482a",
+    "url": "/static/css/chunk-35a0f594.c7330fe9.css"
+  },
+  {
+    "revision": "2221d46ae508aec7482a",
+    "url": "/static/js/chunk-35a0f594.848a5a96.js"
+  },
+  {
+    "revision": "91ae81a249c1f6a1b965",
+    "url": "/static/css/chunk-3a85ec8e.0f97221d.css"
+  },
+  {
+    "revision": "91ae81a249c1f6a1b965",
+    "url": "/static/js/chunk-3a85ec8e.70671b0c.js"
+  },
+  {
+    "revision": "2efa5fa555b13f44c0b9",
+    "url": "/static/css/chunk-4bb12bf2.80aafd40.css"
+  },
+  {
+    "revision": "2efa5fa555b13f44c0b9",
+    "url": "/static/js/chunk-4bb12bf2.5f1ffb2a.js"
+  },
+  {
+    "revision": "47046402beb31911ab0b",
+    "url": "/static/css/chunk-bd8e3ae6.13e232b8.css"
+  },
+  {
+    "revision": "47046402beb31911ab0b",
+    "url": "/static/js/chunk-bd8e3ae6.df27946a.js"
+  },
+  {
+    "revision": "9f72055f43b8fa685fff",
+    "url": "/static/css/chunk-d15e4f08.d323e75e.css"
+  },
+  {
+    "revision": "9f72055f43b8fa685fff",
+    "url": "/static/js/chunk-d15e4f08.0ad767a2.js"
+  },
+  {
+    "revision": "92e3ead1dc2fdb7375a5",
+    "url": "/static/js/chunk-vendors.ca6d8f5e.js"
+  },
+  {
+    "revision": "1cdc8fce9609d07f0e9d8d0bc4b61f8f",
+    "url": "/static/img/btn_google_light_normal_ios.1cdc8fce.svg"
+  },
+  {
+    "revision": "9e5663bc5a0db818f2eaf47a2567f092",
+    "url": "/static/img/library-small.9e5663bc.png"
+  },
+  {
+    "revision": "0be3b7dac46ea85db78f55edc956158e",
+    "url": "/static/img/library-medium.0be3b7da.png"
+  },
+  {
+    "revision": "d8631c5e3e343c2d962091d98add4e88",
+    "url": "/static/img/library-large.d8631c5e.png"
+  },
+  {
+    "revision": "6f16b5de972c2577f9e29f31cdb90f0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5b4176d48547301d284b191f619112f",
+    "url": "/books/9780300139402.jpg"
+  },
+  {
+    "revision": "a3730176db7873697999aaaf404fd26f",
+    "url": "/books/9780300139471.jpg"
+  },
+  {
+    "revision": "88c5df3eaa94e30ae932b9ac36bd505d",
+    "url": "/books/9780300139501.jpg"
+  },
+  {
+    "revision": "de1f8a6b0d7526b7d93cfeed9f5c229c",
+    "url": "/books/9780300139518.jpg"
+  },
+  {
+    "revision": "de5a8786546163d1bd69386798e8e3df",
+    "url": "/books/9780300139594.jpg"
+  },
+  {
+    "revision": "5a32eb5226bb42e2bd666121f4388a0a",
+    "url": "/books/9780300139600.jpg"
+  },
+  {
+    "revision": "b5a9cd3415736d98552b3854e11704fa",
+    "url": "/books/9780300139631.jpg"
+  },
+  {
+    "revision": "69109dfcdc126c88c68dea1817a4d39c",
+    "url": "/books/9780300139693.jpg"
+  },
+  {
+    "revision": "54fff5264770c478f974122e7104bce0",
+    "url": "/books/9780300139709.jpg"
+  },
+  {
+    "revision": "251aa524b3ad471f800c7dbf3891f820",
+    "url": "/books/9780300139723.jpg"
+  },
+  {
+    "revision": "4d04913f2f02527cc66035a52e7ed1a6",
+    "url": "/books/9780300139754.jpg"
+  },
+  {
+    "revision": "44eeef4e38785fea5e1a4ebdb792f5e7",
+    "url": "/books/9780300139778.jpg"
+  },
+  {
+    "revision": "4fa59256715c30af870c3b8b79ea367b",
+    "url": "/books/9780300140248.jpg"
+  },
+  {
+    "revision": "5673084a149f4a98ccb099c0046a2cc7",
+    "url": "/books/9780300140279.jpg"
+  },
+  {
+    "revision": "8e5c0a9504525b25ab5a56ef1c6a4174",
+    "url": "/books/9780300140538.jpg"
+  },
+  {
+    "revision": "23847fa06a03a5277e69e7e85b5ea2a3",
+    "url": "/books/9780300140705.jpg"
+  },
+  {
+    "revision": "a95df703c74095a0c6b1e7f3b99c1cba",
+    "url": "/books/9780300140743.jpg"
+  },
+  {
+    "revision": "4c048304420d6dd10ade1bb5ccb600f5",
+    "url": "/books/9780300140804.jpg"
+  },
+  {
+    "revision": "76d4a46c5d2fbe197b71b4094b652e38",
+    "url": "/books/9780310206071.jpg"
+  },
+  {
+    "revision": "00d81fe7ecc5fd3346080f1916fe8fc8",
+    "url": "/books/9780310206149.jpg"
+  },
+  {
+    "revision": "8d18aa9233fef2a8736726ed8a8ee390",
+    "url": "/books/9780310206378.jpg"
+  },
+  {
+    "revision": "b04d8c526b53e9c1b7b8fda621499550",
+    "url": "/books/9780310213727.jpg"
+  },
+  {
+    "revision": "dcbf9726a8c022f39b696a5a802e1da7",
+    "url": "/books/9780310214427.jpg"
+  },
+  {
+    "revision": "235f5549d38a41cb0d398783af3e65e8",
+    "url": "/books/9780310224587.jpg"
+  },
+  {
+    "revision": "b430ed038d0740dd454f0cb9df38b540",
+    "url": "/books/9780310234944.jpg"
+  },
+  {
+    "revision": "d5f3e6aeffeac83d14eda5903990e03b",
+    "url": "/books/9780310243595.jpg"
+  },
+  {
+    "revision": "6f55c9a4bff7197797c44561832a2335",
+    "url": "/books/9780310243731.jpg"
+  },
+  {
+    "revision": "c10fe92134379dd0ba4bffcfe3a5579e",
+    "url": "/books/9780310243953.jpg"
+  },
+  {
+    "revision": "22efde208b97242a485b9fb8dc11399d",
+    "url": "/books/9780310244165.jpg"
+  },
+  {
+    "revision": "76ef3b2f02a6b572adad8ae3e8e29288",
+    "url": "/books/9780310493501.jpg"
+  },
+  {
+    "revision": "554deee57fd920fcb71bcd6bcc9f0a87",
+    "url": "/books/9780310588320.jpg"
+  },
+  {
+    "revision": "6267017d915abfaa754c3eb720c508c1",
+    "url": "/books/9780567096531.jpg"
+  },
+  {
+    "revision": "1b3f0c68e4eedc423dec6b717c6b796b",
+    "url": "/books/9780664218492.jpg"
+  },
+  {
+    "revision": "1dcc6f773e1d0eadc2007e806d71bf29",
+    "url": "/books/9780664223625.jpg"
+  },
+  {
+    "revision": "7413b4882a53f49d6fadb69c9e08f5a8",
+    "url": "/books/9780687278183.jpg"
+  },
+  {
+    "revision": "df63fdbe30a2279cba0f2fe624abca93",
+    "url": "/books/9780713612530.jpg"
+  },
+  {
+    "revision": "767da5f45c73d0f0fcb433b230e9c62a",
+    "url": "/books/9780785247739.jpg"
+  },
+  {
+    "revision": "4e9878ee2a72ad7a25f05df3f74201ce",
+    "url": "/books/9780785247883.jpg"
+  },
+  {
+    "revision": "eef1f83106eb45ec22f80dea1a09d969",
+    "url": "/books/9780785250135.jpg"
+  },
+  {
+    "revision": "fa817cd66a8564a8c1d0a7877b0980ee",
+    "url": "/books/9780801026447.jpg"
+  },
+  {
+    "revision": "8c9a45cf55fefffa0f26271f9792178f",
+    "url": "/books/9780801026683.jpg"
+  },
+  {
+    "revision": "65cff75eef72020975e755eab26e2fe2",
+    "url": "/books/9780801026720.jpg"
+  },
+  {
+    "revision": "042686bb4f8a71ada2e8f0096cd1ed99",
+    "url": "/books/9780801026737.jpg"
+  },
+  {
+    "revision": "95cb9540223610c69fbe86cbc003866d",
+    "url": "/books/9780801026744.jpg"
+  },
+  {
+    "revision": "c37bb40f4cf5ff0bba3314e8638eaf7e",
+    "url": "/books/9780801026812.jpg"
+  },
+  {
+    "revision": "fe3dcdf7de376e66dd5888ad3eee1f1b",
+    "url": "/books/9780801027031.jpg"
+  },
+  {
+    "revision": "159d0040597f4d4ba5224c335b6e3bfd",
+    "url": "/books/9780801027543.jpg"
+  },
+  {
+    "revision": "cc727cc3a6afce847fd50486efd9b730",
+    "url": "/books/9780801031830.jpg"
+  },
+  {
+    "revision": "f8d670945a390386b3b6aa29bc07f5da",
+    "url": "/books/9780801036316.jpg"
+  },
+  {
+    "revision": "61d1c9730ea3c1b49c5ab66bd8bd223b",
+    "url": "/books/9780801048395.jpg"
+  },
+  {
+    "revision": "dd8c7818d2a8dc675393d00af5041b43",
+    "url": "/books/9780801097447.jpg"
+  },
+  {
+    "revision": "2e7c549b9e4ab17571fe278e92c9f086",
+    "url": "/books/9780802816849.jpg"
+  },
+  {
+    "revision": "d1c6f6b1bc6a0d67e94af7ef20654c3f",
+    "url": "/books/9780802823021.jpg"
+  },
+  {
+    "revision": "de26556df1aaa245a149af3b86b9d3ab",
+    "url": "/books/9780802824462.jpg"
+  },
+  {
+    "revision": "d519ddc7227e7f65d2d29802ccbd6030",
+    "url": "/books/9780802824493.jpg"
+  },
+  {
+    "revision": "2f7ad878bf8ccd4606a4464a797c5c02",
+    "url": "/books/9780802825018.jpg"
+  },
+  {
+    "revision": "6c31193d4f99484e6756bf70e9961f5a",
+    "url": "/books/9780802825049.jpg"
+  },
+  {
+    "revision": "3320ef829bc512a19a1c31831e508d3f",
+    "url": "/books/9780802825094.jpg"
+  },
+  {
+    "revision": "69628a1e76843e7108053248602814e3",
+    "url": "/books/9780802825100.jpg"
+  },
+  {
+    "revision": "e86a785f9d21b2d9ba0af2eeca1f086f",
+    "url": "/books/9780802825162.jpg"
+  },
+  {
+    "revision": "9eb43f6a2972fa951f501e588d436f45",
+    "url": "/books/9780802825230.jpg"
+  },
+  {
+    "revision": "60911af297832505197f55ab0291079f",
+    "url": "/books/9780802825261.jpg"
+  },
+  {
+    "revision": "c83c67b912f0b3c644c8cff784279057",
+    "url": "/books/9780802825278.jpg"
+  },
+  {
+    "revision": "979b3224b6b49fddff3842ef1f116373",
+    "url": "/books/9780802825315.jpg"
+  },
+  {
+    "revision": "76ef0fa3b5ce8cdd1a8204aef12879c7",
+    "url": "/books/9780802825339.jpg"
+  },
+  {
+    "revision": "e96ace91110944ffd2c878c55b782e91",
+    "url": "/books/9780802825438.jpg"
+  },
+  {
+    "revision": "624b1b8135bd494649e5d2e60d3f2ab2",
+    "url": "/books/9780802825452.jpg"
+  },
+  {
+    "revision": "a9e72eb5e7e485e0b5a720ece56c9d71",
+    "url": "/books/9780802827142.jpg"
+  },
+  {
+    "revision": "6d0416cfd28858aed4826aed82260a56",
+    "url": "/books/9780802837295.jpg"
+  },
+  {
+    "revision": "ba6520b2ea11860f5d8235c0f25d7ae9",
+    "url": "/books/9780802837349.jpg"
+  },
+  {
+    "revision": "1643fa96d0cd324e913c6b24d5c4f192",
+    "url": "/books/9780802843418.jpg"
+  },
+  {
+    "revision": "dbd701b0b7675f0cacd2037c64d2963d",
+    "url": "/books/9780802863621.jpg"
+  },
+  {
+    "revision": "b557b4e54a320f3ec874ceb0878ed6f2",
+    "url": "/books/9780802870964.jpg"
+  },
+  {
+    "revision": "3e9aa41b305642db0fe4c7b941b631a0",
+    "url": "/books/9780827606999.jpg"
+  },
+  {
+    "revision": "e8511b7cd4e2d503c85584a942de1409",
+    "url": "/books/9780830824380.jpg"
+  },
+  {
+    "revision": "d6cc577c3bdc9c1a3f95b7022348440b",
+    "url": "/books/9780830829316.jpg"
+  },
+  {
+    "revision": "955183191e7a50596658accdd885218c",
+    "url": "/books/9780830829811.jpg"
+  },
+  {
+    "revision": "9ebfc9dbe0d9e68d9a0da28df99406cd",
+    "url": "/books/9780830829828.jpg"
+  },
+  {
+    "revision": "e5ac7236bc38be337bbd010585fc284e",
+    "url": "/books/9780830829842.jpg"
+  },
+  {
+    "revision": "30a42316adcb1f309a6c0eabbc754205",
+    "url": "/books/9780830829859.jpg"
+  },
+  {
+    "revision": "21543fb61445254673cc53baa010e931",
+    "url": "/books/9780830829866.jpg"
+  },
+  {
+    "revision": "f7275ef1568e554a99f5da00486c9766",
+    "url": "/books/9780830829873.jpg"
+  },
+  {
+    "revision": "0d290ecf18997fdad02c2d0f2797587c",
+    "url": "/books/9780830829910.jpg"
+  },
+  {
+    "revision": "08171d4f33df5f0eed955d929556e855",
+    "url": "/books/9780830829934.jpg"
+  },
+  {
+    "revision": "3f980384b2c2e24f3875e84fdeec71a9",
+    "url": "/books/9780830829958.jpg"
+  },
+  {
+    "revision": "977bb9f424ad22ed8eaefa5ab41625a3",
+    "url": "/books/9780830829965.jpg"
+  },
+  {
+    "revision": "f4dcb56c293662d0cf4b719553fba28f",
+    "url": "/books/9780830829989.jpg"
+  },
+  {
+    "revision": "a61e46fbda29f0b3fc0bfd03194f53f2",
+    "url": "/books/9780830842032.jpg"
+  },
+  {
+    "revision": "405d888c71e62793f889c78c1fc3f91f",
+    "url": "/books/9780830842049.jpg"
+  },
+  {
+    "revision": "48bfa307125d6c95d93310facd40a91c",
+    "url": "/books/9780830842063.jpg"
+  },
+  {
+    "revision": "e4b18fb0b26ed1df36633d665e9e22f7",
+    "url": "/books/9780830842087.jpg"
+  },
+  {
+    "revision": "b117cff52d316cdddff5075f863a8c97",
+    "url": "/books/9780830842117.jpg"
+  },
+  {
+    "revision": "51abc6a77a3104e2e08fa1f6ad3f49da",
+    "url": "/books/9780830842131.jpg"
+  },
+  {
+    "revision": "631691075b71ea39da9884381ce5eceb",
+    "url": "/books/9780830842193.jpg"
+  },
+  {
+    "revision": "a2ca9b86cdbd6f82a2c699997b36da98",
+    "url": "/books/9780849902017.jpg"
+  },
+  {
+    "revision": "a3b5ada5e3e1e0f9ec0776325b2668aa",
+    "url": "/books/9780849902031.jpg"
+  },
+  {
+    "revision": "b2f657990829f8064d2016dd85ab2bbd",
+    "url": "/books/9780849902062.jpg"
+  },
+  {
+    "revision": "6d8d4f94cfecb459ba14191c8c17abcd",
+    "url": "/books/9780849902079.jpg"
+  },
+  {
+    "revision": "e83e03801c0f0e7a6ac1b466c0b1c599",
+    "url": "/books/9780849902086.jpg"
+  },
+  {
+    "revision": "5aa8a500cd105fcf2fe1fc61ea67f869",
+    "url": "/books/9780849902123.jpg"
+  },
+  {
+    "revision": "f64a956c029799c2d332bb172d468eb6",
+    "url": "/books/9780849902130.jpg"
+  },
+  {
+    "revision": "d4dc93c281bb0cd0b3e477dc9c74018a",
+    "url": "/books/9780849902147.jpg"
+  },
+  {
+    "revision": "8fadb859afcfd23a469d223a96859869",
+    "url": "/books/9780849902154.jpg"
+  },
+  {
+    "revision": "b2a8f9d297dbc5bded03cdace5b5dbf5",
+    "url": "/books/9780849902161.jpg"
+  },
+  {
+    "revision": "b63cc08cf4205e73e4ec6e50e2e6fc7e",
+    "url": "/books/9780849902222.jpg"
+  },
+  {
+    "revision": "f85f148252dbbda1073ca648cdb77c00",
+    "url": "/books/9780849902253.jpg"
+  },
+  {
+    "revision": "e9c6c47d55982978e3d10825bc276864",
+    "url": "/books/9780849902291.jpg"
+  },
+  {
+    "revision": "0cb78fb36592304ccdcbcaae8e3f6610",
+    "url": "/books/9780849902390.jpg"
+  },
+  {
+    "revision": "0dbee5c0db903f6dc81bf085d9f7eac6",
+    "url": "/books/9780849902406.jpg"
+  },
+  {
+    "revision": "9da8bdd3e5e8f629acad2b0e12b664eb",
+    "url": "/books/9780849902413.jpg"
+  },
+  {
+    "revision": "8815c102ecd5be5e6331238d1b800a45",
+    "url": "/books/9780849902437.jpg"
+  },
+  {
+    "revision": "1a6ab3e2eac42988eed7a8680d13d9d6",
+    "url": "/books/9780849902444.jpg"
+  },
+  {
+    "revision": "074058ce9fbe7c9119013b92d4c15d08",
+    "url": "/books/9780849902451.jpg"
+  },
+  {
+    "revision": "61c096d56505c0ee1d7fe28b5c0bf93f",
+    "url": "/books/9780849902468.jpg"
+  },
+  {
+    "revision": "5abbe4815e3ffcd177c131e4450bc5a7",
+    "url": "/books/9780849902475.jpg"
+  },
+  {
+    "revision": "4f8431b654f470846e83f76f024850ff",
+    "url": "/books/9780849902482.jpg"
+  },
+  {
+    "revision": "f5cfe8e4de449174f89904916a54c1b6",
+    "url": "/books/9780849902499.jpg"
+  },
+  {
+    "revision": "b03ffcb83bfa2ec4134b5f7d3d0e90e8",
+    "url": "/books/9780849902505.jpg"
+  },
+  {
+    "revision": "4587426e3ffcf63668dcc01e656afa7a",
+    "url": "/books/9780849902512.jpg"
+  },
+  {
+    "revision": "7d7b01cb41be3bc6baa44bf2cbdf9a3d",
+    "url": "/books/9780849908255.jpg"
+  },
+  {
+    "revision": "a23abf4997af1dca5c03e5de9393ed28",
+    "url": "/books/9780849908309.jpg"
+  },
+  {
+    "revision": "2d95fbbc92f1215210d1fdd68c0e9d40",
+    "url": "/books/9780851510743.jpg"
+  },
+  {
+    "revision": "c28f2f620e3dd827d1b8b05f518e31c6",
+    "url": "/books/9780851519821.jpg"
+  },
+  {
+    "revision": "f2314fbcfb305b5c9da31b0a64b0c239",
+    "url": "/books/9780852346273.jpg"
+  },
+  {
+    "revision": "cfed5888cd62a8e30403dc3212ed6ccf",
+    "url": "/books/9780852346563.jpg"
+  },
+  {
+    "revision": "88769a3bebbd10b85652fe59cbda2c83",
+    "url": "/books/9780877842446.jpg"
+  },
+  {
+    "revision": "0a471bf766da015adfe1ee514cfbdebd",
+    "url": "/books/9780877842613.jpg"
+  },
+  {
+    "revision": "f7b00a15424471d6ac7af60cbdb96316",
+    "url": "/books/9780877842767.jpg"
+  },
+  {
+    "revision": "f2dcb22798828b33eec7fb217f59e595",
+    "url": "/books/9780877843108.jpg"
+  },
+  {
+    "revision": "869454acbb5023342dc0ce631a081d03",
+    "url": "/books/9781565632240.jpg"
+  },
+  {
+    "revision": "aae467f631c8d11644ef5f495688691e",
+    "url": "/books/9781596381971.jpg"
+  },
+  {
+    "revision": "98b328f5b57fa76cccecf2e1e4635db6",
+    "url": "/books/9781629954585.jpg"
+  },
+  {
+    "revision": "50af483020b4de0479d9159e2559ea1f",
+    "url": "/books/9781781910627.jpg"
+  },
+  {
+    "revision": "13fcb0728dae1ebc6a5c788a9cfbf4ed",
+    "url": "/books/9781845503635.jpg"
+  },
+  {
+    "revision": "363d513ef24599e6b48a95c09b60e168",
+    "url": "/books/9781850492474.jpg"
+  },
+  {
+    "revision": "89ef34b6c6c5fb495e9ecea61fbab735",
+    "url": "/books/9781857928617.jpg"
+  },
+  {
+    "revision": "d0d7ce91d2de3b29dcdc0d1e6d7b9079",
+    "url": "/books/9781870855280.jpg"
+  },
+  {
+    "revision": "55ad2e898060c53897ad7fe9ecaac8d8",
+    "url": "/books/9781905679027.jpg"
+  },
+  {
+    "revision": "a4151c5020642af83f41e876a66dc846",
+    "url": "/img/no-book-cover.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "9a658139fa1328b233c7ba802fe039cd",
+    "url": "/books/9780802821799.jpg"
+  },
+  {
+    "revision": "e16dcca7ce02dba869dafb2bdadeafd2",
+    "url": "/books/9780849902321.jpg"
+  },
+  {
+    "revision": "65113ce26b80ba2ec144c00cd2c1cae9",
+    "url": "/books/9780849902314.jpg"
+  },
+  {
+    "revision": "7bdc6b7758fcabca11ca623819ea9f38",
+    "url": "/books/9780849902345.jpg"
+  }
+];
